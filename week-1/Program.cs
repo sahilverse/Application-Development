0ab83a8a9ae35ba6d782ccdeddffc9f3f2a3c974
Sahil Dahal <denenme49@gmail.com>
2025-11-19 19:16:41 +0545

    Week-1 workshop

---
+++ week-1/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void PrintPersonalInfo(string name, int age, string city)
+    {
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"City: {city}");
+    }
+
+
+    static void Main(string[] args)
+    {
+        PrintPersonalInfo("Sahil", 24, "Itahari");
+        Quote quote = new();
+
+        Console.WriteLine("\nMotivational Quote of the Day:");
+        quote.DisplayQuote();
+    }
+}
---
+++ week-1/Quote.cs
@@ -0,0 +1,10 @@
+class Quote
+{
+    private string motivationalQuoteOfTheDay = "The only way to do great work is to love what you do. - Steve Jobs";
+
+
+    public void DisplayQuote()
+    {
+        Console.WriteLine(motivationalQuoteOfTheDay);
+    }
+}
